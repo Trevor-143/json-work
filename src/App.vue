b7268ba0c3699e5fbb3b9475a949994d48740c4b
Arapu Trevor <105975921+Trevor-143@users.noreply.github.com>
2023-05-18 16:54:27 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h1>Human Actions</h1>
+    <div v-for="(item, index) in data" :key="index" class="action">
+      <img :src="item.image" :alt="item.name">
+      <h3> by: {{item.by}} </h3>
+      <h3> {{item.name}} </h3>
+      <p> {{item.content}} </p>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import { ref } from "vue";
+import Data from "./data/info.json"
+
+export default {
+  setup() {
+    
+    const data = ref([])
+
+    data.value = Data
+
+    return {
+      data
+    }
+  }
+}
+</script>
+<style>
+
+.action {
+  padding: 2rem;
+  background: #eee;
+  margin: 2rem 10%;
+  border-radius: 3rem;
+}
+.action img {
+  width: 150px;
+  height: 150px;
+  object-fit: cover;
+  border-radius: 50%;
+}
+
+</style>
